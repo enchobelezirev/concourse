32bd922d55e5df798a0f86113f897af96728cc8b
Encho Belezirev <encho.belezirev@sap.com>
2018-05-22 10:08:58 +0300

    Try to update the deploy-service

--- oq.tests/test_scenarios/XS Tests/Update Deploy Service/run.sh
+++ oq.tests/test_scenarios/XS Tests/Update Deploy Service/run.sh
@@ -4,7 +4,7 @@ cd "${script_dir}";
 source "${script_dir}/env.sh"
 cd 
 pwd
-bash "test_scripts/update_deploy_service.sh"
+bash "/workspace/test_scripts/update_deploy_service.sh"
 if [[ $? -ne 0 ]] ; then echo "EXECUTION FAILED!";
 mailx -S smtp=mail.sap.corp -s "EXECUTION FAILED! ./SL4XS2/XS Deploy Service OQ Tests/Prepare Deploy Service and Scripts/Update Deploy Service" -v encho.belezirev@sap.com <<<./SL4XS2/XS Deploy Service OQ Tests/Prepare Deploy Service and Scripts/Update Deploy Service;
 exit 1; fi
