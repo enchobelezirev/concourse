8f2671e3e7ac7fcb77fa9b258a6cc350b91f48ad
Encho Belezirev <9638278+enchobelezirev@users.noreply.github.com>
2018-05-22 18:16:59 +0300

    Update update_deploy_service.sh

--- oq.tests/test_scripts/update_deploy_service.sh
+++ oq.tests/test_scripts/update_deploy_service.sh
@@ -2,6 +2,8 @@ export PARENT_DIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd );
 echo $PARENT_DIR
 ls -a -l $PARENT_DIR
 source ${PARENT_DIR}/test_functions.sh
+echo "Printing the env"
+env
 generate_local_executable "$0"
 set -e
 update_deploy_service_content
