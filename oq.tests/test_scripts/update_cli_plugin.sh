05779185094322c1da4ffe1a341ff012117f27ec
Encho Belezirev <9638278+enchobelezirev@users.noreply.github.com>
2018-05-23 12:55:47 +0300

    Update update_cli_plugin.sh

--- oq.tests/test_scripts/update_cli_plugin.sh
+++ oq.tests/test_scripts/update_cli_plugin.sh
@@ -2,7 +2,7 @@ export PARENT_DIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd );
 source ${PARENT_DIR}/test_functions.sh
 generate_local_executable "$0"
 set -e
-if [[ $RUNTIME == "xs" ]]; then
+#if [[ $RUNTIME == "xs" ]]; then
   update_xs_cli_plugin
 #else
  # update_cf_cli_plugin
