77f598ef7e97f84364d6d20aa117118fd9152402
Encho Belezirev <encho.belezirev@sap.com>
2018-05-28 15:50:36 +0300

    Printing env as part of deployment

--- oq.tests/test_scenarios/XS Tests/Opaque Tokens/Deploy/run.sh
+++ oq.tests/test_scenarios/XS Tests/Opaque Tokens/Deploy/run.sh
@@ -3,6 +3,7 @@ script_dir="$(dirname -- "$(realpath -- "${BASH_SOURCE[0]}")")"
 cd "${script_dir}";
 source "${script_dir}/env.sh"
 cd 
+env
 bash "/workspace/test_scripts/test_deploy.sh"
 if [[ $? -ne 0 ]] ; then echo "EXECUTION FAILED!";
 mailx -S smtp=mail.sap.corp -s "EXECUTION FAILED! ./SL4XS2/XS Deploy Service OQ Tests/Test Suites/Functionality Tests/Parallel Set 1/Optional MTA Resources/With existing-service resource type not optional/Deploy" -v dimitar.donchev@sap.com <<<./SL4XS2/XS Deploy Service OQ Tests/Test Suites/Functionality Tests/Parallel Set 1/Optional MTA Resources/With existing-service resource type not optional/Deploy;
--- oq.tests/test_scenarios/XS Tests/Opaque Tokens/Login and Target/run.sh
+++ oq.tests/test_scenarios/XS Tests/Opaque Tokens/Login and Target/run.sh
@@ -3,6 +3,7 @@ script_dir="$(dirname -- "$(realpath -- "${BASH_SOURCE[0]}")")"
 cd "${script_dir}";
 source "${script_dir}/env.sh"
 cd 
+env
 bash "/workspace/test_scripts/login_and_target_space.sh"
 if [[ $? -ne 0 ]] ; then echo "EXECUTION FAILED!";
 mailx -S smtp=mail.sap.corp -s "EXECUTION FAILED! ./SL4XS2/XS Deploy Service OQ Tests/Test Suites/Functionality Tests/Parallel Set 1/Optional MTA Resources/Login and Target/Login and Target" -v dimitar.donchev@sap.com <<<./SL4XS2/XS Deploy Service OQ Tests/Test Suites/Functionality Tests/Parallel Set 1/Optional MTA Resources/Login and Target/Login and Target;
