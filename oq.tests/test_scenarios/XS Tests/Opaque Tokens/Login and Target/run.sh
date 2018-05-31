12660c7c34014fc0b232468b47d1c67608e75558
Encho Belezirev <encho.belezirev@sap.com>
2018-05-31 17:03:34 +0300

    Try to use the downloaded git repo

--- oq.tests/test_scenarios/XS Tests/Opaque Tokens/Login and Target/run.sh
+++ oq.tests/test_scenarios/XS Tests/Opaque Tokens/Login and Target/run.sh
@@ -4,6 +4,9 @@ cd "${script_dir}";
 source "${script_dir}/env.sh"
 cd 
 env
+ls -a -l oq-test-git-repo/oq.tests/test_scripts
+ls -a -l oq-test-git-repo/oq.tests/
+ls -a -l oq-test-git-repo/
 bash "oq-test-git-repo/oq.tests/test_scripts/login_and_target_space.sh"
 if [[ $? -ne 0 ]] ; then echo "EXECUTION FAILED!";
 mailx -S smtp=mail.sap.corp -s "EXECUTION FAILED! ./SL4XS2/XS Deploy Service OQ Tests/Test Suites/Functionality Tests/Parallel Set 1/Optional MTA Resources/Login and Target/Login and Target" -v dimitar.donchev@sap.com <<<./SL4XS2/XS Deploy Service OQ Tests/Test Suites/Functionality Tests/Parallel Set 1/Optional MTA Resources/Login and Target/Login and Target;
