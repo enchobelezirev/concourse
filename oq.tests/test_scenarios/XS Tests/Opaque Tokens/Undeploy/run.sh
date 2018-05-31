054e1e6eda72155e8933adc61f41f97cb12af89b
Encho Belezirev <encho.belezirev@sap.com>
2018-05-31 17:55:34 +0300

    Hopefully the final changes

--- oq.tests/test_scenarios/XS Tests/Opaque Tokens/Undeploy/run.sh
+++ oq.tests/test_scenarios/XS Tests/Opaque Tokens/Undeploy/run.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 script_dir="$(dirname -- "$(realpath -- "${BASH_SOURCE[0]}")")"
 cd "${script_dir}";
-source "${script_dir}/env.sh"
+# source "${script_dir}/env.sh"
 cd 
-bash "/workspace/test_scripts/test_undeploy.sh"
+bash "${TEST_WORKING_DIRECTORY}/oq-test-git-repo/oq.tests/test_scripts/test_undeploy.sh"
 if [[ $? -ne 0 ]] ; then echo "EXECUTION FAILED!";
 mailx -S smtp=mail.sap.corp -s "EXECUTION FAILED! ./SL4XS2/XS Deploy Service OQ Tests/Test Suites/Functionality Tests/Parallel Set 1/One-off Tasks Scenario/Test With Failing Tasks/Undeploy" -v dimitar.donchev@sap.com <<<./SL4XS2/XS Deploy Service OQ Tests/Test Suites/Functionality Tests/Parallel Set 1/One-off Tasks Scenario/Test With Failing Tasks/Undeploy;
 exit 1; fi
