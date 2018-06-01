c6cbb17d81ae92b77f8e70e3f604867475db19d3
Encho Belezirev <encho.belezirev@sap.com>
2018-06-01 08:18:21 +0300

    Fix deployment

--- oq.tests/test_scenarios/XS Tests/Opaque Tokens/Deploy/env.sh
+++ oq.tests/test_scenarios/XS Tests/Opaque Tokens/Deploy/env.sh
@@ -5,6 +5,6 @@ if [ -f "${parent_dir}/env.sh" ] ; then
         source "${parent_dir}/env.sh";
 fi
 # TODO: add those to the params
-export STEP_PATH="${STEP_PATH}/Deploy"
-export APP_LOCATION="/workspace/test_resources/opaque-tokens-resources"
-export EXPECTED_APPLICATIONS="ztana"
+# export STEP_PATH="${STEP_PATH}/Deploy"
+# export APP_LOCATION="/workspace/test_resources/opaque-tokens-resources"
+# export EXPECTED_APPLICATIONS="ztana"
